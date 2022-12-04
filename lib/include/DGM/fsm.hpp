4dbec5238563a24530fcb10e693416e94c717726
Jakub Neruda <nerudaj@email.cz>
2022-12-04 03:02:45 +0100

    Fixed unexpected tokens in fsm.hpp

--- lib/include/DGM/fsm.hpp
+++ lib/include/DGM/fsm.hpp
@@ -2,7 +2,7 @@
 
 #include "classes/Fsm.hpp"
 #include "classes/FsmBuilder.hpp"
-#include "classes/FsmDecorators.hpp">"
+#include "classes/FsmDecorators.hpp"
 #include "classes/FsmFactory.hpp"
 #include "classes/FsmLoader.hpp"
-#include "classes/FsmJsonLoader.hpp">"
+#include "classes/FsmJsonLoader.hpp"
